13d50222f0411b26021867c8d922a0e3a9ee2822
Jason Glassbrook <28660371+jason-glassbrook@users.noreply.github.com>
2020-01-22 12:20:30 -0500

    rename /users/router <- /users/user-router

--- server.js
+++ server.js
@@ -1,13 +1,11 @@
 const express = require('express');
 const helmet = require('helmet');
 
-const UserRouter = require('./users/user-router.js');
-
 const server = express();
 
 server.use(helmet());
 server.use(express.json());
 
-server.use('/api/users', UserRouter);
+server.use('/api/users', require('./users/router'));
 
 module.exports = server;
